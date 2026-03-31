e5d303aebf7f190bf0561641f00e109e8dd5c5b5
Kai Deines <deines.kl@outlook.de>
2026-04-01 00:22:27 +0200

    feat: configure environment settings for development and production

--- user-auth-frontend/src/app/auth.ts
+++ user-auth-frontend/src/app/auth.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 import { jwtDecode } from 'jwt-decode';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Auth {
-  private apiUrl = 'http://127.0.0.1:8000';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
