ab94e2232810c7fd1b6796959b9932c3894adfcb
Kai Deines <deines.kl@outlook.de>
2026-04-06 20:56:44 +0200

    add household api url

--- user-auth-frontend/src/environments/environment.development.ts
+++ user-auth-frontend/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8000',
+  production: false,
+  apiUrl: 'http://localhost:8000',
+  householdApiUrl: 'http://localhost:8001',
 };
