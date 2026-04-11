654116c2599f40eef4985347f76d8353ae31ab97
Kai Deines <deines.kl@outlook.de>
2026-04-11 19:15:17 +0200

    fix: update database connection strings and add volume mappings in docker-compose

--- household-service/src/household_service/database.py
+++ household-service/src/household_service/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 # English comment: Connection string for the persistent SQLite file
-SQLALCHEMY_DATABASE_URL = "sqlite:///./households.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./household.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
