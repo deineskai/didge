f7042912072f7c7e2c25270d8f681e31f943e224
Kai Deines <deines.kl@outlook.de>
2026-04-26 18:35:15 +0200

    feat: update recipe component to display diets and tags using Badge component

--- frontend/src/app/features/recipe/recipe.ts
+++ frontend/src/app/features/recipe/recipe.ts
@@ -2,15 +2,17 @@ import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CulinaryService } from '../../core/culinary-service';
 import { ContentPageLayout } from '../../shared/layouts/content-page-layout/content-page-layout';
+import { Badge } from '../../shared/components/badge/badge';
 
 @Component({
   selector: 'app-recipe',
-  imports: [ContentPageLayout],
+  imports: [ContentPageLayout, Badge],
   templateUrl: './recipe.html',
   styleUrl: './recipe.css',
 })
 export class Recipe implements OnInit {
   recipe: any = {};
+  diets: string[] = [];
   private cdr = inject(ChangeDetectorRef);
 
   constructor(
@@ -29,15 +31,34 @@ export class Recipe implements OnInit {
   }
 
   loadRecipe(id: number) {
-    //this.recipeService.getRecipeById(id).subscribe(data => {
-    //  this.recipe = data;
-    //});
     this.culinaryService.getRecipeById(id).subscribe({
       next: (data) => {
         this.recipe = data;
+        this.diets = this.getDietsAsStrings();
         this.cdr.detectChanges();
       },
     });
     console.log(this.recipe);
+  }
+
+  getDietsAsStrings(): string[] {
+    const diets = this.recipe.diets;
+
+    let keys = Object.keys(diets).filter((key) => diets[key] === true);
+
+    if (keys.includes('vegan')) {
+      keys = keys.filter((k) => k !== 'vegetarian');
+    }
+
+    return keys.map((key) => this.formatDietName(key));
+  }
+
+  formatDietName(key: string): string {
+    if (!key) return '';
+
+    return key
+      .split('_')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) // Macht 'Gluten' und 'Free' daraus
+      .join('-');
   }
 }
